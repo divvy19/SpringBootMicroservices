8f721655767af71967c628268ee30e5e25493284
Divya Bais <divya.bais@xecurify.com>
2022-03-29 11:36:25 +0530

    Adding springboot project

---
+++ src/main/java/com/example/demo/MicroservicesMinikubeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesMinikubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesMinikubeApplication.class, args);
+	}
+
+}
